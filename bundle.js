36278cbfbb119108b8c52ef6c2f3bddd7f69791d
liuz <liuzhuan@qingsongchou.com>
2017-12-14 11:07:36 +0800

    adding rollup build scripts

--- bundle.js
+++ bundle.js
@@ -1,3 +1,9 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.canvasLoading = factory());
+}(this, (function () { 'use strict';
+
 /**
  * 环形加载进度构造函数
  * @param {Object} options 
@@ -7,9 +13,9 @@
  * @param {string} color - 颜色
  * @param {number} startAngle - 开始角度
  */
-export default function Loader(options = {}) {
+function Loader(options = {}) {
   this.ratio = 0;
-  this.container = options.container || 'body'
+  this.container = options.container || 'body';
   this.width = options.width || 100;
   this.height = options.height || 100;
   this.cx = this.width / 2;
@@ -23,20 +29,20 @@ export default function Loader(options = {}) {
 Loader.prototype.update = function(val) {
   this.ratio = val;
   if (!this.context) {
-    this.context = this.createContext()
+    this.context = this.createContext();
   }
 
-  this.drawSlice(val)
-}
+  this.drawSlice(val);
+};
 
 // 创建绘图的上下文
 Loader.prototype.createContext = function() {
-  const canvasContainer = document.querySelector(this.container)
-  canvasContainer.innerHTML = `<canvas width="${this.width}" height="${this.height}" class="js-canvas"></canvas>`
-  const canvas = document.querySelector(this.container + ' .js-canvas')
-  const ctx = canvas.getContext('2d')
+  const canvasContainer = document.querySelector(this.container);
+  canvasContainer.innerHTML = `<canvas width="${this.width}" height="${this.height}" class="js-canvas"></canvas>`;
+  const canvas = document.querySelector(this.container + ' .js-canvas');
+  const ctx = canvas.getContext('2d');
   return ctx
-}
+};
 
 Loader.prototype.drawSlice = function(ratio) {
   const ctx = this.context;
@@ -48,20 +54,24 @@ Loader.prototype.drawSlice = function(ratio) {
   const radius = this.radius;
   const startAngle = this.startAngle;
 
-  ctx.clearRect(0, 0, w, h)
+  ctx.clearRect(0, 0, w, h);
   
   // draw the outer circle line
-  ctx.beginPath()
-  ctx.strokeStyle = color
-  ctx.arc(cx, cy, radius, 0, Math.PI * 2, true)
-  ctx.stroke()
-  ctx.closePath()
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.arc(cx, cy, radius, 0, Math.PI * 2, true);
+  ctx.stroke();
+  ctx.closePath();
   
-  ctx.beginPath()
-  ctx.fillStyle = color
+  ctx.beginPath();
+  ctx.fillStyle = color;
   const endAngle = ratio * Math.PI * 2 + startAngle;
-  ctx.moveTo(cx, cy)
-  ctx.arc(cx, cy, radius, startAngle, endAngle, false)
-  ctx.lineTo(cx, cy)
-  ctx.fill()
-}
+  ctx.moveTo(cx, cy);
+  ctx.arc(cx, cy, radius, startAngle, endAngle, false);
+  ctx.lineTo(cx, cy);
+  ctx.fill();
+};
+
+return Loader;
+
+})));
